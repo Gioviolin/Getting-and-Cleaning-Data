c1c11b2cab3e500ae629450e6e251487b46714d7
Giordano Bruno A. Ronconi <giordano.antoniazzi@gmail.com>
2015-10-17 17:51:13 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -20,20 +20,20 @@ XTest$participants <- SubjectTest[, 1]
 
 ## Assignment Tasks
 
-# Merges the training and the test sets to create one data set.
+# 1) Merges the training and the test sets to create one data set.
 
 DataSet <- rbind(XTrain, XTest)
 duplicated(colnames(DataSet))
 DataSet <- DataSet[, !duplicated(colnames(DataSet))]
 
-# Extracts only the measurements on the mean and standard deviation for each measurement.
+# 2) Extracts only the measurements on the mean and standard deviation for each measurement.
 
 Mean <- grep("mean()", names(DataSet), value = FALSE, fixed = TRUE)
 OnlyMeans <- DataSet[Mean]
 STD <- grep("std()", names(DataSet), value = FALSE)
 OnlySTD <- DataSet[STD]
 
-# Uses descriptive activity names to name the activities in the data set
+# 3) Uses descriptive activity names to name the activities in the data set
 
 DataSet$activities <- as.character(DataSet$activities)
 DataSet$activities[DataSet$activities == 1] <- "Walking"
@@ -44,7 +44,9 @@ DataSet$activities[DataSet$activities == 5] <- "Standing"
 DataSet$activities[DataSet$activities == 6] <- "Laying"
 DataSet$activities <- as.factor(DataSet$activities)
 
-# Appropriately labels the data set with descriptive variable names.
+# 4) Appropriately labels the data set with descriptive variable names.
+
+# Labeling variables
 
 names(DataSet) <- gsub("Acc", "Accelerator", names(DataSet))
 names(DataSet) <- gsub("Mag", "Magnitude", names(DataSet))
@@ -89,7 +91,7 @@ DataSet$activities[DataSet$activities == 29] <- "Participant 29"
 DataSet$activities[DataSet$activities == 30] <- "Participant 30"
 DataSet$participants <- as.factor(DataSet$participants)
 
-#Create a tidy data set
+# 5) Create a tidy data set with the average of each variable for each activity and each subject
 
 DataSet.dt <- data.table(DataSet)
 
